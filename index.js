24e648964921e601d480e9387741b210978a4696
vaishnavi <vaishnavisv001@gmail.com>
2018-06-04 16:01:32 +0530

    refactor to pull date wise feeds

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ var _ = require('lodash');
 //var _ = require('lodash/core');
 // Load the FP build for immutable auto-curried iteratee-first data-last methods.
 var fp = require('lodash/fp');
- 
+
 // Load method categories.
 var array = require('lodash/array');
 var object = require('lodash/fp/object');
- 
+
 // Cherry-pick methods for smaller browserify/rollup/webpack bundles.
 var at = require('lodash/at');
 var curryN = require('lodash/fp/curryN');
-//Import and add btoa 
+//Import and add btoa
 var btoa = require('btoa');
-//Import and add cron 
+//Import and add cron
 //var cron = require('cron');
 //Import request to make http requests
 const request = require ("request");
@@ -42,12 +42,12 @@ var dbpassword = process.env.dbpassword; //for production environment
 //var dbpassword = 'admin';//for development environment
 //The complete url of database host with protocol
 var url = dbprotocol+dbhost; //for production environment
-	//var url = 'http://localhost:5984';//for development environment
+//	var url = 'http://localhost:5984';//for development environment
 //Import database feeds from environment variable
 var db = process.env.feeddbname; //for production environment
 	//var db ='feeds';//for development environment
-//Import client url to set cors	
-	
+//Import client url to set cors
+
 //var clienturl='localhost:4200';
 		var clienturl=process.env.clienturl;//for production environment
 
@@ -56,17 +56,17 @@ var db = process.env.feeddbname; //for production environment
 
 /*  The MIT License (MIT)
 	Copyright (c) 2014-2017 Dave Winer
-	
+
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-	
+
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-	
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -79,7 +79,7 @@ var db = process.env.feeddbname; //for production environment
 var cron = require('cron');
 
 var job1 = new cron.CronJob({
-  cronTime: '* * */3 * * 0-6',
+  cronTime: '0 */30 * * * *',
   onTick: function() {
   	//console.log('running every minute 1, 2, 4 and 5');
     console.log('job 1 ticked');
@@ -88,18 +88,18 @@ var job1 = new cron.CronJob({
     	});
 
     },
-  start: false,
+  start: true	,
   timeZone: 'America/Los_Angeles'
 });
 //console.log('job1 status', job1.running); // job1 status undefined
 job1.start();
 console.log('job1 status running', job1.running); // job1 status undefined
-//Get all user's subscription links and check for the last 	
+//Get all user's subscription links and check for the last
 function getUsersSubscriptionsLinks(callback){
 	//options to get the user subsrciptions from the user's database
 	const options = {
 	  method: 'GET',
-	  uri: 'http://stage-couch.test.openrun.net/supertest%24mediamonitor18/_all_docs?include_docs=true',
+	  uri: url + '/' + db +'/_design/feeds/_view/link?reduce=true&group_level=2',
 	  headers: {
 	    'Content-Type': 'application/json',
 	    'Authorization': 'Basic '+btoa(dbusername+':'+dbpassword)
@@ -108,90 +108,120 @@ function getUsersSubscriptionsLinks(callback){
 request(options, function(err, res, body) {
 		if(body != undefined){
 			//console.log(JSON.parse(body).rows)
-		if(JSON.parse(body).rows.length > 0){	
+		if(JSON.parse(body).rows.length > 0){
 
 		//Parse the result to json and store the user's link in an array
 		JSON.parse(body).rows.map(link=>{
 			//console.log("all",link.key[0],link.key[1]);
-			link.doc.metadata.map(userlink=>{
-				//console.log("all",userlink)
+			//link.doc.metadata.map(userlink=>{
+				//console.log("all",	userlink)
 				//Hack only to update the mediamonitor subscriptions
 				//Check if the xml rss link is null
-				if(userlink.xmlurl == null){
+				/*if(userlink.xmlurl == null){
 					var feedlink=userlink.link;
-				}	
+				}
 				else{
 					feedlink=userlink.xmlurl;
-				}
-				//Get feeds from the db by passing the feedname 
-				getfeedsFromdb(userlink,function(err,feedsFromDb){
-					  		console.log(feedsFromDb.length);
-				  	//Get feeds from the newsrack by passing the link as parameter
-					getFeed (feedlink,function (err, feedItems) {
-						//console.log(feedItems);
-					 //Check if feeds from database exists	
-					  if(feedsFromDb.length>0){	
-						if (!err) {
-							
-							var feedstoUpdate = differenceOfFeeds(feedsFromDb,feedItems);
-							
-							if(feedstoUpdate.length > 0){
-								
-								updateDB(feedstoUpdate,link.key[1],function(err,response){
+				}*/
+				//Get feeds from the newsrack by passing the link as parameter
+					getFeed (link.key[0],function (err, feedItems) {
+						//console.log(err)
+						console.log("klin",link.key[0]);
+						if (!err && feedItems[0]!= undefined && feedItems[feedItems.length-1]!=undefined) {
+								getSortedfeeds(feedItems,function (err,sortedFeeds) {
+										//console.log("sorted",sortedFeeds);
+
+								//Get feeds from the db by passing the feedname
+									getfeedsFromdb(link.key[1],sortedFeeds[feedItems.length-1].pubdate,sortedFeeds[0].pubdate,function(err,feedsFromDb){
+					  		//console.log(feedsFromDb.length);
+								//console.log("feedslink",link.key[0]);
+
+					 			//Check if feeds from database exists
+					  		if(feedsFromDb.length>0){
+
+
+									var feedstoUpdate = differenceOfFeeds(feedsFromDb,sortedFeeds);
+
+									if(feedstoUpdate.length > 0){
+									//console.log(feedstoUpdate.length);;
+										updateDB(feedstoUpdate,link.key[1],function(err,response){
 									//console.log(response);
-									if(response){
-										callback(undefined,true);
+											if(response){
+													callback(undefined,true);
+											}
+										})
 									}
-								})
-							}
-							else{
-								callback(undefined,false);
-							}	
-						}
-					  }	
-					});
-				  
-				})
-			  
+									else{
+										callback(undefined,false);
+									}
+								}
+
+							});
+						})
+
+					}
 				});
+
 			});
-	  	}	
+	  	}
 	  	}
 		});
 
 }
+//Function to fetch sorted feeds
+function getSortedfeeds(feedItems,callback){
+	var sortedFeeds = feedItems.sort(function(a, b) {
+     // console.log("datea",a,b)
+      return new Date(b.pubdate).getTime() - new Date(a.pubdate).getTime()
+    });
+		callback(undefined,sortedFeeds);
+		//console.log("datea",sortedFeeds)
+}
 
 //Fumction to get feeds from database on feedname
-function getfeedsFromdb(feedname,callback) {
-	//console.log(encodeURIComponent(feedname))
-	/*request(url+'/' + db + '/_design/feeds/_view/categoryfeeds?key="'+encodeURIComponent(feedname)+'"', function(err, res, body) {
-		console.log("catte",feedname,JSON.parse(body).rows.length);
-		if(body != undefined){
-			callback(undefined,JSON.parse(body).rows);				
+function getfeedsFromdb(feedname,startkey,endkey,callback) {
+	//console.log(feedname,startkey,endkey)
+  //var d = new Date();
+   //var previousweek= new Date(d.getTime() -   72 * 60 * 60 * 1000);
+	 //console.log(url+'/' + db + '/_design/feeds/_view/feedsondate?startkey=["'+startkey.toISOString()+'"]&endkey=["'+endkey.toISOString()+'"]');
+	request(url+'/' + db + '/_design/feeds/_view/feedsondate?startkey=["'+startkey.toISOString()+'"]&endkey=["'+endkey.toISOString()+'"]', function(err, res, body) {
+			//console.log("catte",);
+		if(body != undefined && JSON.parse(body).error!== 'query_parse_error'){
+				console.log("uncategroisedfeeds",body);
+			//callback(undefined,JSON.parse(body).rows);
+
+				var categorisedfeeds = JSON.parse(body).rows.filter(catname=>{
+        	return catname.value.feedname === feedname;
+      	})
+				console.log("categroisedfeeds",feedname,categorisedfeeds.length);
+     		callback(undefined,JSON.parse(body).rows);
+
+
+
 		}
-			  
-	});*/
+
+	});
 	//console.log(feedname);
 	//Check if metacategory is defined and fetch the feeds
-		if(feedname.categories[0] == undefined){
+		/*if(feedname.categories[0] == undefined){
 			//console.log(feed);
 			request(url+'/' + db + '/_design/feeds/_view/categoryfeeds?key="'+feed.doc.feedname+'"', function(err, res, body) {
 				console.log("catte",JSON.parse(body).rows.length);
 				if(body != undefined){
-					callback(undefined,JSON.parse(body).rows);				
+					callback(undefined,JSON.parse(body).rows);
 				}
-					  
+
 			});
 		}
 		else{
 			request(url+'/' + db + '/_design/feeds/_view/metacategoryfeeds?key="'+feedname.categories[0]+'"', function(err, res, body) {
 				console.log("meta",JSON.parse(body).rows.length);
 				if(body != undefined){
-					callback(undefined,JSON.parse(body).rows);				
+					callback(undefined,JSON.parse(body).rows);
 				}
-					  
+
 			});
-		}
+		}*/
 }
 //Function to update the database
 function updateDB(data,feedname,callback){
@@ -213,23 +243,21 @@ function updateDB(data,feedname,callback){
 //Function to get the difference feeds from the feeds array from database and feeds array from newsrack
 function differenceOfFeeds(feedsarray,feedItems) {
 		//console.log("feedarr length",feedsarray.length);
-	if(feedsarray.length>0){	
+	if(feedsarray.length>0){
 	var databasefeeds = feedsarray.map(value=>{
-		//
-		delete value.value._id;
-		delete value.value._rev;	
-		//console.log(value);
+
 		return value.value;
 
 	});
-	var res = _.differenceBy(feedItems,databasefeeds,'title');
-	//console.log("result",res.length)
-	
-	return res;
+  // console.log(feedItems[0],databasefeeds[0]);
+	var res = _.differenceBy(feedItems,databasefeeds,'link');
+		console.log("result",res.length)
+
+		return res;
 
 
 	}
-	
+
 }
 //cors settings
 app.use(function(req, res, next) {
@@ -247,7 +275,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', true);
   return next();
-}); 
+});
 //Pull new feeds from newsrack
 app.get('/',cors(),function(req, res) {
 	var syncStatus;
@@ -255,21 +283,21 @@ app.get('/',cors(),function(req, res) {
 		console.log(response);
 
 		if(response==true){
-			console.log("up",res.headersSent)
+			//console.log("up",res.headersSent)
 			if(!res.headersSent){
 				res.writeHead(201, { 'Content-Type': 'text/plain' });
 				res.end('ok');
 			}
 		}
 		else if(response == false){
-			console.log("not up",res.headersSent)
+			//console.log("not up",res.headersSent)
 			if(!res.headersSent){
 				res.writeHead(304, { 'Content-Type': 'text/plain' });
 				res.end('ok');
 			}
 		}
 
-	}); 
+	});
 
 });
 
@@ -328,4 +356,3 @@ function getFeed (urlfeed, callback) {
 
 
 app.listen(port, () => console.log('Example app listening on port '))
-
