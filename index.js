ff8709080c0d9ffd68991aeb2c4f6565d99af2db
salus-sage <salus.sage@gmail.com>
2018-10-17 16:47:58 +0530

    add line numbers to console log

--- index.js
+++ index.js
@@ -52,6 +52,33 @@ var db = process.env.feeddbname; //for production environment
 	var clienturl=process.env.clienturl;//for production environment
 
 	var clienturlwithprotocol= '*'; //dbprotocol + clienturl;
+
+// add line numbers in console log
+['log', 'warn', 'error'].forEach((methodName) => {
+  const originalMethod = console[methodName];
+  console[methodName] = (...args) => {
+    let initiator = 'unknown place';
+    try {
+      throw new Error();
+    } catch (e) {
+      if (typeof e.stack === 'string') {
+        let isFirst = true;
+        for (const line of e.stack.split('\n')) {
+          const matches = line.match(/^\s+at\s+(.*)/);
+          if (matches) {
+            if (!isFirst) { // first line - current function
+                            // second line - caller (what we are looking for)
+              initiator = matches[1];
+              break;
+            }
+            isFirst = false;
+          }
+        }
+      }
+    }
+    originalMethod.apply(console, [...args, '\n', `  at ${initiator}`]);
+  };
+});
 	console.log(clienturlwithprotocol);
 
 /*  The MIT License (MIT)
